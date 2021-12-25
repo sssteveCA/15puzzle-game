a4043995dee704fff514972bfe76c4669cc5c6b5
sssteveCA <steppuggio@gmail.com>
2021-12-25 18:00:15 +0100

    salvataggio.php Db Table check added

--- oggetti/salvataggio.php
+++ oggetti/salvataggio.php
@@ -57,6 +57,7 @@ SQL;
     private $connesso; //se l'oggetto ha stabilito la connessione con il database
     private $tabella; //tabella MySql dei salvataggi
     private $query; //ultima query inviata dall'oggetto
+    private $queries; //lista di query SQL eseguite
     private $id;
     private $idg; //id del giocatore che ha creato il salvataggio
     private $data; //data in cui il salvataggio è stato creato
@@ -79,12 +80,20 @@ SQL;
         $mysqlUser=isset($ingresso['mysqlUser'])? $ingresso['mysqlUser']:Salvataggio::USERNAME;
         $mysqlPass=isset($ingresso['mysqlPass'])? $ingresso['mysqlPass']:Salvataggio::PASSWORD;
         $mysqlDb=isset($ingresso['mysqlDb'])? $ingresso['mysqlDb']:Salvataggio::DATABASE;   
-        $this->h = new mysqli($mysqlHost,$mysqlUser,$mysqlPass,$mysqlDb);
+        $this->h = new mysqli($mysqlHost,$mysqlUser,$mysqlPass);
+        $this->query = '';
+        $this->queries = array();
         if($this->h->connect_errno !== 0){
             throw new Exception("Connessione a MySql fallita: ".$this->h->connect_error);
         }
+        if(!$this->createDb($mysqlDb)){
+            throw new Exception("Errore durante il controllo del database");
+        }
+        $this->h->select_db($mysqlDb);
         $this->tabella = isset($ingresso['tabella'])? $ingresso['tabella']:Salvataggio::TABLE;
-        $this->query = '';
+        if(!$this->createTable()){
+            throw new Exception("Errore durante il controllo della tabella");
+        }    
         $this->error = null;
         $this->h->set_charset("utf8mb4");
         $this->connesso = true;
@@ -138,6 +147,7 @@ SQL;
     }
     public function getSpostamenti(){return $this->spostamenti;}
     public function getQuery(){return $this->query;}
+    public function getQueries(){return $this->queries;}
     public function getErrno(){return $this->errno;}
     public function getError(){
         switch($this->errno){
@@ -173,6 +183,52 @@ SQL;
 
     public function setSequenza($sequenza){
         $this->sequenza = implode(" ",$sequenza);
+    }
+
+    //crea il database se non esiste
+    private function createDb($db){
+        $ok = false;
+        $this->query = <<<SQL
+CREATE DATABASE IF NOT EXISTS {$db};
+SQL;
+        $this->queries[] = $this->query;
+        $create = $this->h->query($this->query);
+        if($create !== false)
+            $ok = true;
+        return $ok;
+    }
+
+    //crea la tabella se non esiste
+    private function createTable(){
+        $ok = false;
+        $this->query = <<<SQL
+SHOW TABLES LIKE '{$this->tabella}';
+SQL;
+        $this->queries[] = $this->query;
+        $show = $this->h->query($this->query);
+        if($show !== false){
+            if($show->num_rows == 0){
+                $this->query = <<<SQL
+CREATE TABLE `{$this->tabella}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idg` int(11) NOT NULL COMMENT 'id del giocatore a cui appartiene il salvataggio',
+  `data` datetime NOT NULL COMMENT 'data in cui il salvataggio è stato creato',
+  `slot` int(11) NOT NULL COMMENT 'dove il giocatore vuole salvare la partita',
+  `sequenza` varchar(100) NOT NULL COMMENT 'posizione di ciascuna tessera',
+  `tempo` varchar(30) NOT NULL COMMENT 'tempo passato pfino al momento in cui  stato creato il salvataggio',
+  `spostamenti` int(11) NOT NULL COMMENT 'numero di tessere spostate fino al momento in cui è stato creato il salvataggio',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4
+SQL;
+            $this->queries[] = $this->query;
+            $create = $this->h->query($this->query);
+            if($create !== false)
+                $ok = true;
+            }//if($show->num_rows == 0){
+            else
+                $ok = true;
+        }//if($show !== false){        
+        return $ok;
     }
 
     //ottengo le informazioni del salvataggio
